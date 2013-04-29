6312c782226735996b8789a4b9a26cef5df40f62
c.rodriguez <c.rodriguez@globant.com>
2013-04-29 08:12:55 -0500

    Agregados entites: Product y Campagin (al product le falta el tipo de dato para almacenar la imagen)

--- AddsIncGAE/src/com/globant/gaetraining/addsincgae/model/Campaign.java
+++ AddsIncGAE/src/com/globant/gaetraining/addsincgae/model/Campaign.java
@@ -1,5 +1,71 @@
 package com.globant.gaetraining.addsincgae.model;
 
+import java.util.Date;
+
+import javax.jdo.annotations.IdGeneratorStrategy;
+import javax.jdo.annotations.PersistenceCapable;
+import javax.jdo.annotations.Persistent;
+import javax.jdo.annotations.PrimaryKey;
+
+import com.google.appengine.api.datastore.Key;
+
+@PersistenceCapable
 public class Campaign {
+
+	@PrimaryKey
+	@Persistent(valueStrategy = IdGeneratorStrategy.IDENTITY)
+	private Key key;
+
+	@Persistent
+	private String name;
+
+	@Persistent
+	private Date startDate;
+
+	@Persistent
+	private Date endDate;
+
+	@Persistent
+	private Product product;
+
+	public Key getKey() {
+		return key;
+	}
+
+	public void setKey(Key key) {
+		this.key = key;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public Date getStartDate() {
+		return startDate;
+	}
+
+	public void setStartDate(Date startDate) {
+		this.startDate = startDate;
+	}
+
+	public Date getEndDate() {
+		return endDate;
+	}
+
+	public void setEndDate(Date endDate) {
+		this.endDate = endDate;
+	}
+
+	public Product getProduct() {
+		return product;
+	}
+
+	public void setProduct(Product product) {
+		this.product = product;
+	}
 
 }
--- AddsIncGAE/src/com/globant/gaetraining/addsincgae/model/Product.java
+++ AddsIncGAE/src/com/globant/gaetraining/addsincgae/model/Product.java
@@ -1,5 +1,91 @@
 package com.globant.gaetraining.addsincgae.model;
 
+import java.util.List;
+
+import javax.jdo.annotations.IdGeneratorStrategy;
+import javax.jdo.annotations.Persistent;
+import javax.jdo.annotations.PrimaryKey;
+
+import com.google.appengine.api.datastore.Key;
+
 public class Product {
+
+	@PrimaryKey
+	@Persistent(valueStrategy = IdGeneratorStrategy.IDENTITY)
+	private Key key;
+
+	@Persistent
+	private String name;
+
+	@Persistent
+	private List<String> productFamily;
+
+	@Persistent
+	private String shortDescription;
+
+	@Persistent
+	private String longDescription;
+
+	@Persistent
+	private String url;
+
+	@Persistent
+	private String country;
+
+	public Key getKey() {
+		return key;
+	}
+
+	public void setKey(Key key) {
+		this.key = key;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public List<String> getProductFamily() {
+		return productFamily;
+	}
+
+	public void setProductFamily(List<String> productFamily) {
+		this.productFamily = productFamily;
+	}
+
+	public String getShortDescription() {
+		return shortDescription;
+	}
+
+	public void setShortDescription(String shortDescription) {
+		this.shortDescription = shortDescription;
+	}
+
+	public String getLongDescription() {
+		return longDescription;
+	}
+
+	public void setLongDescription(String longDescription) {
+		this.longDescription = longDescription;
+	}
+
+	public String getUrl() {
+		return url;
+	}
+
+	public void setUrl(String url) {
+		this.url = url;
+	}
+
+	public String getCountry() {
+		return country;
+	}
+
+	public void setCountry(String country) {
+		this.country = country;
+	}
 
 }
