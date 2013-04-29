d87890f7489784196e3470ce3b0e2d91d17f679b
c.rodriguez <c.rodriguez@globant.com>
2013-04-29 15:38:04 -0500

    Correjida la firma para que trabaje con la entidad correcta

--- AddsIncGAE/src/com/globant/gaetraining/addsincgae/daos/CampaignDao.java
+++ AddsIncGAE/src/com/globant/gaetraining/addsincgae/daos/CampaignDao.java
@@ -5,11 +5,10 @@ import java.util.List;
 import org.springframework.stereotype.Repository;
 
 import com.globant.gaetraining.addsincgae.model.Campaign;
-import com.globant.gaetraining.addsincgae.model.Customer;
 import com.google.appengine.api.datastore.Key;
 
 @Repository
-public class CampaignDao extends GenericDao<Customer> {
+public class CampaignDao extends GenericDao<Campaign> {
 
 	public List<Campaign> findActiveByCustomerKey(Key customerKey) {
 		return null;
