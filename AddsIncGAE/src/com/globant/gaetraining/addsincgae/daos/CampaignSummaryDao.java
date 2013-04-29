e5f3ced6763c5f5d8ced051a27e3fcb9e19641b9
c.rodriguez <c.rodriguez@globant.com>
2013-04-29 15:33:38 -0500

    Correjido el nombre de las clases de Campana creadas por mi, les había puesto campaing en lugar de campaign :(

--- AddsIncGAE/src/com/globant/gaetraining/addsincgae/daos/CampaignSummaryDao.java
+++ AddsIncGAE/src/com/globant/gaetraining/addsincgae/daos/CampaignSummaryDao.java
@@ -2,10 +2,13 @@ package com.globant.gaetraining.addsincgae.daos;
 
 import java.util.List;
 
+import org.springframework.stereotype.Repository;
+
 import com.globant.gaetraining.addsincgae.model.CampaignSummary;
 import com.google.appengine.api.datastore.Key;
 
-public class CampaingSummaryDao extends GenericDao<CampaignSummary> {
+@Repository
+public class CampaignSummaryDao extends GenericDao<CampaignSummary> {
 
 	public List<CampaignSummary> findByCustomerKey(Key customerKey) {
 		return null;
--- AddsIncGAE/test/com/globant/gaetraining/addsincgae/daos/CampaignSummaryDaoTest.java
+++ AddsIncGAE/test/com/globant/gaetraining/addsincgae/daos/CampaignSummaryDaoTest.java
@@ -11,16 +11,16 @@ import com.globant.gaetraining.addsincgae.model.CampaignSummary;
 import com.google.appengine.tools.development.testing.LocalDatastoreServiceTestConfig;
 import com.google.appengine.tools.development.testing.LocalServiceTestHelper;
 
-public class CampaingSummaryDaoTest {
+public class CampaignSummaryDaoTest {
 
 	private final LocalServiceTestHelper helper = new LocalServiceTestHelper(
 			new LocalDatastoreServiceTestConfig());
-	private CampaingSummaryDao dao;
+	private CampaignSummaryDao dao;
 
 	@Before
 	public void setUp() {
 		this.helper.setUp();
-		this.dao = new CampaingSummaryDao();
+		this.dao = new CampaignSummaryDao();
 	}
 
 	@After
@@ -29,22 +29,22 @@ public class CampaingSummaryDaoTest {
 	}
 
 	@Test
-	public void persistCampaingSummaryTest() {
+	public void persistCampaignSummaryTest() {
 
-		CampaignSummary campaingSummary = new CampaignSummary();
+		CampaignSummary campaignSummary = new CampaignSummary();
 
-		campaingSummary = this.dao.persist(campaingSummary);
-		Assert.assertNotNull(campaingSummary);
+		campaignSummary = this.dao.persist(campaignSummary);
+		Assert.assertNotNull(campaignSummary);
 
 	}
 
 	@Test
 	public void findByValidKeyTest() {
 
-		CampaignSummary campaingSummary = new CampaignSummary();
-		campaingSummary = this.dao.persist(campaingSummary);
+		CampaignSummary campaignSummary = new CampaignSummary();
+		campaignSummary = this.dao.persist(campaignSummary);
 
-		CampaignSummary result = this.dao.findByKey(campaingSummary.getKey(),
+		CampaignSummary result = this.dao.findByKey(campaignSummary.getKey(),
 				CampaignSummary.class);
 		Assert.assertNotNull(result);
 
@@ -53,9 +53,9 @@ public class CampaingSummaryDaoTest {
 	@Test
 	public void findAllTest() {
 
-		CampaignSummary campaingSummary = new CampaignSummary();
-		campaingSummary.setTotalHits(10);
-		campaingSummary = this.dao.persist(campaingSummary);
+		CampaignSummary campaignSummary = new CampaignSummary();
+		campaignSummary.setTotalHits(10);
+		campaignSummary = this.dao.persist(campaignSummary);
 
 		List<CampaignSummary> result = this.dao.findAll(CampaignSummary.class);
 
