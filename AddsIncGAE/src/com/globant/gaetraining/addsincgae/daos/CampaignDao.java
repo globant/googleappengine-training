31ee79b4e76c5ae51e9e25c9f29c5cbc2fb1523b
c.rodriguez <c.rodriguez@globant.com>
2013-04-30 10:38:49 -0500

    Agregado query para consultar las campañas activas para un customer, se agrega tambien su respectivo test

--- AddsIncGAE/src/com/globant/gaetraining/addsincgae/daos/CampaignDao.java
+++ AddsIncGAE/src/com/globant/gaetraining/addsincgae/daos/CampaignDao.java
@@ -1,16 +1,43 @@
 package com.globant.gaetraining.addsincgae.daos;
 
+import java.util.Date;
 import java.util.List;
+
+import javax.jdo.Query;
 
 import org.springframework.stereotype.Repository;
 
 import com.globant.gaetraining.addsincgae.model.Campaign;
+import com.globant.gaetraining.addsincgae.model.Customer;
 import com.google.appengine.api.datastore.Key;
 
 @Repository
 public class CampaignDao extends GenericDao<Campaign> {
 
+	/**
+	 * find all active {@link Campaign} associated to a {@link Customer}
+	 * 
+	 * @param customerKey
+	 *            {@link Key} of the customer
+	 * @return {@link List} with the {@link Campaign} found or null
+	 */
+	@SuppressWarnings("unchecked")
 	public List<Campaign> findActiveByCustomerKey(Key customerKey) {
-		return null;
+
+		List<Campaign> results = null;
+
+		Query query = this.getPM().newQuery(Campaign.class);
+
+		query.setFilter("customerKey == customerKeyParam");
+		query.setFilter("startDate < currentDateParam");
+		query.setFilter("endDate > currentDateParam");
+
+		query.declareImports("import java.util.Date");
+		query.declareParameters("String customerKeyParam, Date currentDateParam");
+
+		results = (List<Campaign>) query.execute(customerKey, new Date());
+
+		return results;
+
 	}
 }
--- AddsIncGAE/test/com/globant/gaetraining/addsincgae/daos/CampaignDaoTest.java
+++ AddsIncGAE/test/com/globant/gaetraining/addsincgae/daos/CampaignDaoTest.java
@@ -1,6 +1,8 @@
 package com.globant.gaetraining.addsincgae.daos;
 
 import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.GregorianCalendar;
 import java.util.List;
 
 import org.junit.After;
@@ -9,6 +11,7 @@ import org.junit.Before;
 import org.junit.Test;
 
 import com.globant.gaetraining.addsincgae.model.Campaign;
+import com.globant.gaetraining.addsincgae.model.Customer;
 import com.globant.gaetraining.addsincgae.model.Product;
 import com.google.appengine.tools.development.testing.LocalDatastoreServiceTestConfig;
 import com.google.appengine.tools.development.testing.LocalServiceTestHelper;
@@ -18,11 +21,13 @@ public class CampaignDaoTest {
 	private final LocalServiceTestHelper helper = new LocalServiceTestHelper(
 			new LocalDatastoreServiceTestConfig());
 	private CampaignDao dao;
+	private CustomerDao daoCustomer;
 
 	@Before
 	public void setUp() {
 		this.helper.setUp();
 		this.dao = new CampaignDao();
+		this.daoCustomer = new CustomerDao();
 	}
 
 	@After
@@ -61,6 +66,35 @@ public class CampaignDaoTest {
 	}
 
 	@Test
+	public void persistCampaignCustomerAndProducts() {
+
+		Customer customer = new Customer();
+		customer.setName("Test Man!!");
+		customer = this.daoCustomer.persist(customer);
+
+		Campaign camp = new Campaign();
+		camp.setName("MockParent");
+		camp.setProduct(new ArrayList<Product>());
+		camp.setCustomerKey(customer.getKey());
+
+		Product prod;
+		for (int i = 0; i < 100; i++) {
+			prod = new Product();
+			prod.setName(Integer.toString(i));
+			prod.setShortDescription("SD");
+			prod.setLongDescription("Long description");
+			prod.setUrl("http://jkjk.com/");
+			camp.getProduct().add(prod);
+		}
+		Campaign cmp = dao.persist(camp);
+		Assert.assertNotNull(camp.getKey());
+		Assert.assertEquals(cmp.getProduct().size(), 100);
+		for (int i = 0; i < 100; i++) {
+			Assert.assertNotNull(cmp.getProduct().get(i).getKey());
+		}
+	}
+
+	@Test
 	public void findByValidKeyTest() {
 		Campaign campaign = new Campaign();
 		campaign = this.dao.persist(campaign);
@@ -77,5 +111,46 @@ public class CampaignDaoTest {
 		List<Campaign> result = this.dao.findAll(Campaign.class);
 		Assert.assertNotNull(result);
 
+	}
+
+	@Test
+	public void findActiveByCustomerKey() {
+
+		Customer customer = new Customer();
+		customer.setName("Test Man!!");
+		customer = this.daoCustomer.persist(customer);
+
+		// Active campaign
+		Campaign campA = new Campaign();
+		campA.setName("MockParent");
+		campA.setProduct(new ArrayList<Product>());
+		campA.setCustomerKey(customer.getKey());
+		campA.setStartDate(new GregorianCalendar(2010, 01, 01).getTime());
+		campA.setEndDate(new GregorianCalendar(2018, 01, 01).getTime());
+		
+		// Active campaign
+		Campaign campB = new Campaign();
+		campB.setName("MockParent");
+		campB.setProduct(new ArrayList<Product>());
+		campB.setCustomerKey(customer.getKey());
+		campB.setStartDate(new GregorianCalendar(2010, 01, 01).getTime());
+		campB.setEndDate(new GregorianCalendar(2018, 01, 01).getTime());
+		
+		// inactive campaign
+		Campaign campC = new Campaign();
+		campC.setName("MockParent");
+		campC.setProduct(new ArrayList<Product>());
+		campC.setCustomerKey(customer.getKey());
+		campC.setStartDate(new GregorianCalendar(2010, 01, 01).getTime());
+		campC.setEndDate(new GregorianCalendar(2013, 01, 01).getTime());
+
+		dao.persist(campA);
+		dao.persist(campB);
+		dao.persist(campC);
+		
+		List<Campaign> campaigns = dao.findActiveByCustomerKey(customer
+				.getKey());
+		
+		Assert.assertEquals(2, campaigns.size());
 	}
 }
