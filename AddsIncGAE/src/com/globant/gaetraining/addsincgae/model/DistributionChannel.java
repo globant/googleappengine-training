0a9789be21898d411091b093c00a24ecbb8ef6a3
Leonardo Contreras <leonardo.contreras@gmail.com>
2013-04-30 08:46:44 -0500

    Initial data for development

--- AddsIncGAE/src/com/globant/gaetraining/addsincgae/model/DistributionChannel.java
+++ AddsIncGAE/src/com/globant/gaetraining/addsincgae/model/DistributionChannel.java
@@ -1,8 +1,69 @@
 package com.globant.gaetraining.addsincgae.model;
 
+import javax.jdo.annotations.IdGeneratorStrategy;
 import javax.jdo.annotations.PersistenceCapable;
+import javax.jdo.annotations.Persistent;
+import javax.jdo.annotations.PrimaryKey;
+
+import com.google.appengine.api.datastore.Key;
 
 @PersistenceCapable
 public class DistributionChannel {
+	
+	@PrimaryKey
+	@Persistent(valueStrategy = IdGeneratorStrategy.IDENTITY)
+	private Key key;
+	
+	@Persistent
+	private String name;
+	
+	@Persistent
+	private String mediaType;
+	
+	@Persistent
+	private String template;
+	
+	@Persistent
+	private String country;
+
+	public Key getKey() {
+		return key;
+	}
+
+	public void setKey(Key key) {
+		this.key = key;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getMediaType() {
+		return mediaType;
+	}
+
+	public void setMediaType(String mediaType) {
+		this.mediaType = mediaType;
+	}
+
+	public String getTemplate() {
+		return template;
+	}
+
+	public void setTemplate(String template) {
+		this.template = template;
+	}
+
+	public String getCountry() {
+		return country;
+	}
+
+	public void setCountry(String country) {
+		this.country = country;
+	}
 
 }
--- AddsIncGAE/src/com/globant/gaetraining/addsincgae/services/HomeService.java
+++ AddsIncGAE/src/com/globant/gaetraining/addsincgae/services/HomeService.java
@@ -1,5 +1,65 @@
 package com.globant.gaetraining.addsincgae.services;
 
+import java.util.ArrayList;
+import java.util.Calendar;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.globant.gaetraining.addsincgae.daos.CampaignDao;
+import com.globant.gaetraining.addsincgae.daos.DistributionChannelDao;
+import com.globant.gaetraining.addsincgae.model.Campaign;
+import com.globant.gaetraining.addsincgae.model.DistributionChannel;
+import com.globant.gaetraining.addsincgae.model.Product;
+import com.google.appengine.api.datastore.Key;
+import com.google.appengine.api.datastore.KeyFactory;
+
+@Service
 public class HomeService {
+	
+	@Autowired
+	CampaignDao campaignDao;
+
+	@Autowired
+	DistributionChannelDao distChannelDao;
+	
+	public void populate(){
+		//DistChannel
+		DistributionChannel distChannel = new DistributionChannel();
+		Key keyDist = KeyFactory.createKey("DistributionChannel", "mock_distributionchannel");
+		distChannel.setKey(keyDist);
+		distChannel.setName("Mockito");
+		distChannel.setMediaType("TV");
+		distChannel.setTemplate("<div><h4>{{name}}</h4><p>{{longdesc}}</p></div>");
+		distChannelDao.persist(distChannel);
+		
+		
+		//Campaign
+		Campaign campaign = new Campaign();
+		Key keyCamp = KeyFactory.createKey("Campaign", "mock_campaign");
+		campaign.setKey(keyCamp);
+		campaign.setName("Mock");
+		Calendar cal = Calendar.getInstance();
+		cal.add(Calendar.DATE, 30);
+		campaign.setEndDate(cal.getTime());
+		cal.add(Calendar.YEAR, -1);
+		campaign.setStartDate(cal.getTime());
+		campaign.setDistributionChannelKeys(new ArrayList<Key>());
+		campaign.getDistributionChannelKeys().add(keyDist);
+		campaign.setProduct(new ArrayList<Product>());
+		Key keyProduct = KeyFactory.createKey("Product", "mock_product");
+		Product product = new Product();
+		product.setKey(keyProduct);
+		product.setName("Mockiproduct");
+		product.setShortDescription("Short Desc");
+		product.setLongDescription("The longer description here");
+		product.setUrl("http://mock.globant.com/");
+		campaignDao.persist(campaign);
+		
+		
+		
+	}
+
+
 
 }
