16f545d7c54c6e57d7077c9d01e39c136f947883
c.rodriguez <c.rodriguez@globant.com>
2013-04-30 08:45:59 -0500

    Agregado el dao para las consultas de productsummary

---
+++ AddsIncGAE/src/com/globant/gaetraining/addsincgae/daos/ProductSummaryDao.java
@@ -0,0 +1,10 @@
+package com.globant.gaetraining.addsincgae.daos;
+
+import org.springframework.stereotype.Repository;
+
+import com.globant.gaetraining.addsincgae.model.ProductSummary;
+
+@Repository
+public class ProductSummaryDao extends GenericDao<ProductSummary> {
+
+}
