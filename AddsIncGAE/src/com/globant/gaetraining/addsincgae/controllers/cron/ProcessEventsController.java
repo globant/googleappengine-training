b88d15d02d9851e2f472f100f538b32ae11c0e85
Leonardo Contreras <leonardo.contreras@gmail.com>
2013-04-30 17:18:46 -0500

    Added cron, backend and controller for backend processing

---
+++ AddsIncGAE/src/com/globant/gaetraining/addsincgae/controllers/cron/ProcessEventsController.java
@@ -0,0 +1,13 @@
+package com.globant.gaetraining.addsincgae.controllers.cron;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class ProcessEventsController {
+
+	@RequestMapping("/process_events")
+	public void processEventsBatch(){
+		
+	}
+}
