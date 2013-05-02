a0343bcf7b94edde6a55337b2707976cddd43d16
c.rodriguez <c.rodriguez@globant.com>
2013-05-02 15:34:00 -0500

    Correjida documetación

--- AddsIncGAE/src/com/globant/gaetraining/addsincgae/daos/CampaignSummaryDao.java
+++ AddsIncGAE/src/com/globant/gaetraining/addsincgae/daos/CampaignSummaryDao.java
@@ -6,16 +6,21 @@ import org.springframework.stereotype.Repository;
 
 import com.globant.gaetraining.addsincgae.model.Campaign;
 import com.globant.gaetraining.addsincgae.model.CampaignSummary;
+import com.globant.gaetraining.addsincgae.model.DistributionChannel;
+import com.globant.gaetraining.addsincgae.model.DistributionChannelSummary;
+import com.globant.gaetraining.addsincgae.model.Product;
+import com.globant.gaetraining.addsincgae.model.ProductSummary;
 
 @Repository
 public class CampaignSummaryDao extends GenericDao<CampaignSummary> {
 
 	/**
-	 * Find a {@link Campaign} by his campaign id. Load his products
+	 * Find a {@link CampaignSummary} by his campaign id. Load his
+	 * {@link ProductSummary} and {@link DistributionChannelSummary}
 	 * 
 	 * @param campaignId
 	 *            Id of the {@link Campaign}
-	 * @return {@link Campaign}
+	 * @return {@link CampaignSummary}
 	 */
 	public CampaignSummary findByIdWithProductsAndDistrChannelsSummaries(
 			Object campaignId) {
