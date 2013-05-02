ab3fd988afc1aa5c85ca33d150ce409ac4597adb
jslozano <juan.lozano@globant.com>
2013-05-02 15:04:12 -0300

    Merge

--- AddsIncGAE/src/com/globant/gaetraining/addsincgae/controllers/HomeController.java
+++ AddsIncGAE/src/com/globant/gaetraining/addsincgae/controllers/HomeController.java
@@ -26,17 +26,6 @@ public class HomeController {
 		model.put("greet", greeting);
 		System.out.println("Pasa HomeController");
 		homeService.populate();
-		String[] prods = {
-				"mock_product1__1","mock_product1__2",
-				"mock_product2__1","mock_product2__2",
-				"mock_product3__1","mock_product3__2",
-				"mock_product4__1","mock_product4__2",
-				"mock_product5__1","mock_product5__2",
-				"mock_product6__1","mock_product6__2",
-				"mock_product7__1","mock_product7__2",
-				"mock_product8__1","mock_product8__2"};
-		String[] channels = {"mock_distributionchannel2","mock_distributionchannel"};
-		homeService.dummyEventTasks(channels,prods );
 		
 		return "home";
 	}
