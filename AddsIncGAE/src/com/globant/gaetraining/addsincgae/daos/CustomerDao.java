6bdd219e38fd51f9f0c7e34d69b3affa1bf2cca9
c.rodriguez <c.rodriguez@globant.com>
2013-05-02 09:19:21 -0500

    Agregado método para consultar todos los customers

--- AddsIncGAE/src/com/globant/gaetraining/addsincgae/daos/CustomerDao.java
+++ AddsIncGAE/src/com/globant/gaetraining/addsincgae/daos/CustomerDao.java
@@ -1,5 +1,7 @@
 package com.globant.gaetraining.addsincgae.daos;
 
+import java.util.List;
+
 import org.springframework.stereotype.Repository;
 
 import com.globant.gaetraining.addsincgae.model.Customer;
@@ -7,5 +9,13 @@ import com.globant.gaetraining.addsincgae.model.Customer;
 @Repository
 public class CustomerDao extends GenericDao<Customer> {
 
+	/**
+	 * Find all customers
+	 * 
+	 * @return {@link List} of all {@link Customer}
+	 */
+	public List<Customer> findAll() {
+		return super.findAll(Customer.class);
+	}
 
 }
