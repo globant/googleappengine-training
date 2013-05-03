0aa16694124307744eaa560348f500df6b5b29f6
Jorge Spinsanti <jorge.spinsanti@globant.com>
2013-05-03 12:31:15 -0300

    Create basic product

--- AddsIncGAE/src/com/globant/gaetraining/addsincgae/controllers/ProductController.java
+++ AddsIncGAE/src/com/globant/gaetraining/addsincgae/controllers/ProductController.java
@@ -5,17 +5,26 @@ import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+import com.globant.gaetraining.addsincgae.model.Campaign;
 import com.globant.gaetraining.addsincgae.model.Product;
+import com.globant.gaetraining.addsincgae.services.CampaignService;
 import com.globant.gaetraining.addsincgae.services.ProductService;
+import com.google.appengine.api.datastore.KeyFactory;
 
 @Controller
 public class ProductController {
 	
 	@Autowired
 	private ProductService productService;
+	
+	@Autowired
+	private CampaignService campaignService;
 	
 	@RequestMapping(value = "/products", method = RequestMethod.GET)
 	public String getCustomers(Map<String, Object> model) {
@@ -27,5 +36,30 @@ public class ProductController {
 		return "ProductList";
 	}
 	
+	@RequestMapping(value="/addProduct", method = RequestMethod.GET)
+	public String add(Model model){
+		//TODO Assign a campaign defined by the user
+		Campaign campaign = null;
+		List<Campaign> campaigns = campaignService.getCampaigns();
+		if (campaigns.isEmpty()) {
+			campaign = new Campaign();
+			campaign.setKey(KeyFactory.createKey("campaign", 1L));
+		} else {
+			campaign = campaigns.get(0);
+		}
+		
+		Product p = new Product(campaign);
+		model.addAttribute("product", p);
+		
+		return "AddProduct";
+	}
+	
+	@RequestMapping(value = "/products", method = RequestMethod.POST)
+	public String addProduct(@ModelAttribute("product") Product product,
+			ModelMap model) {
 
+		productService.addProduct(product);
+
+		return "redirect:/products";
+	}
 }
