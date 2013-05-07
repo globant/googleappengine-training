5ba16b1b1d37f48705aaa28e42787a4e5f117e2e
c.rodriguez <c.rodriguez@globant.com>
2013-05-07 15:29:58 -0500

    Agregado método para convertir una lsita de product summary en un hashmap. La key del map es la Key del producto asociado al summary

--- AddsIncGAE/src/com/globant/gaetraining/addsincgae/daos/ProductSummaryDao.java
+++ AddsIncGAE/src/com/globant/gaetraining/addsincgae/daos/ProductSummaryDao.java
@@ -1,10 +1,32 @@
 package com.globant.gaetraining.addsincgae.daos;
 
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 import org.springframework.stereotype.Repository;
 
+import com.globant.gaetraining.addsincgae.model.Product;
 import com.globant.gaetraining.addsincgae.model.ProductSummary;
+import com.google.appengine.api.datastore.Key;
 
 @Repository
 public class ProductSummaryDao extends GenericDao<ProductSummary> {
 
+	/**
+	 * Convert a {@link List} of {@link ProductSummary} into a {@link Map}. The
+	 * Key of the map is the Key of the {@link Product}
+	 * 
+	 * @param products
+	 *            {@link List} of {@link ProductSummary} to convert
+	 * @return {@link Map} of {@link ProductSummary} converted
+	 */
+	public Map<Key, ProductSummary> convertToMapWithProductKey(
+			List<ProductSummary> products) {
+		Map<Key, ProductSummary> productsMap = new HashMap<Key, ProductSummary>();
+		for (ProductSummary productSummary : products) {
+			productsMap.put(productSummary.getProductKey(), productSummary);
+		}
+		return productsMap;
+	}
 }
